0913352dd2db4fb9c1c02df25bec7c72cb8617a4
Dmitriy Pozdeev <dmitriyweb@inbox.ru>
2019-05-28 13:14:40 +0500

    Props lesson 29 complete

--- src/components/todo-list.js
+++ src/components/todo-list.js
@@ -3,8 +3,10 @@ import TodoListItem from './todo-list-item'
 const TodoList = () => {
     return (
     <ul>
-      <li><TodoListItem /></li>
-      <li><TodoListItem /></li> 
+      <li><TodoListItem label="Drink Cofee" /></li>
+      <li><TodoListItem 
+        label="Build React App" 
+        important/></li> 
     </ul>
     );
 };
